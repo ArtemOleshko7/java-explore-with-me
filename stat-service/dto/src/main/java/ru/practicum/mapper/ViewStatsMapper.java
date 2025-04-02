11439de142bd3f3e22e3cd8827bc1a37ff18568e
Artem Oleshko <freedom2323232323@gmail.com>
2025-04-02 18:22:08 +0300

    second part

--- stat-service/dto/src/main/java/ru/practicum/mapper/ViewStatsMapper.java
+++ stat-service/dto/src/main/java/ru/practicum/mapper/ViewStatsMapper.java
@@ -15,7 +15,7 @@ public class ViewStatsMapper implements RowMapper<ViewStats> {
         return ViewStats.builder()
                 .app(rs.getString("app"))
                 .uri(rs.getString("uri"))
-                .hits(rs.getLong("hits"))
+                .hits((int) rs.getLong("hits"))
                 .build();
     }
 }
